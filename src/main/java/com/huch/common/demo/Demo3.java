a2209817886e6896a9b16a738e15d9e34475855b
huch.mac <228922025@qq.com>
2019-11-26 02:28:00 +0800

    doc

---
+++ src/main/java/com/huch/common/demo/Demo3.java
@@ -0,0 +1,13 @@
+package com.huch.common.demo;
+
+/**
+ * @author huchanghua
+ * @create 2019-11-26-00:26
+ */
+public class Demo3 {
+
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}

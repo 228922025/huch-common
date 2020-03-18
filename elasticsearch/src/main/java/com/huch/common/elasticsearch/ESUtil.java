fe1baa4ca8a99f7bab9207e69cbc0b82e6add07e
huch-huawei <228922025@qq.com>
2020-03-18 11:44:54 +0800

    更新elasticsearch为7.6.1

--- elasticsearch/src/main/java/com/huch/common/elasticsearch/ESUtil.java
+++ elasticsearch/src/main/java/com/huch/common/elasticsearch/ESUtil.java
@@ -1,58 +1,94 @@
 package com.huch.common.elasticsearch;
 
-import com.alibaba.fastjson.JSON;
+import org.apache.http.HttpHost;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 
-import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author huchanghua
  * @create 2019-12-10-21:46
  */
 public class ESUtil {
+    private static final int TIME_OUT = 5 * 60 * 1000;
+    private static final String HTTP_SCHEME = "http";
 
     /**
-     * 创建elasticsearch TransportClient 实例
+     * 创建 RestClientBuilder
      * @param host
      * @param port
-     * @param clustername
      * @return
      */
-    public static TransportClient createTransportClient(String host, Integer port, String clustername){
-        TransportClient client = null;
-        Settings settings = Settings.builder().put("cluster.name", clustername).build();
-        TransportAddress transportAddress = new InetSocketTransportAddress(new InetSocketAddress(host, port));
-        client = TransportClient.builder()
-                .settings(settings).build()
-                .addTransportAddress(transportAddress);
+    public static RestClientBuilder createRestClientBuilder(String host, Integer port) {
+        return RestClient.builder(new HttpHost(host, port, HTTP_SCHEME));
+    }
+
+    /**
+     * 创建 RestClientBuilder
+     * param address
+     * @return
+     */
+    public static RestClientBuilder createRestClientBuilder(HttpHost... address) {
+        return RestClient.builder(address);
+    }
+
+    /**
+     * 创建 RestHighLevelClient 客户端
+     * @param host
+     * @param port
+     * @return
+     */
+    public static RestHighLevelClient createRestHighLevelClient(String host, Integer port) {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(host, port, "http")));
         return client;
     }
 
     /**
-     * 解析查询数据到List<Map<String, Object>
+     * 创建 RestHighLevelClient 客户端
+     * @param address
+     * @return
+     */
+    public static RestHighLevelClient createRestHighLevelClient(HttpHost... address) {
+        RestHighLevelClient client = new RestHighLevelClient(createRestClientBuilder(address));
+        return client;
+    }
+
+    /**
+     * 将查询结果解析成ListMaps
      * @param response
      * @return
      */
-    public static List<Map<String, Object>> parseToMap(SearchResponse response){
-        SearchHits searchHits = response.getHits();
-        SearchHit[] hits = searchHits.getHits();
-        List list = new ArrayList();
-        for (int i = 0; i < hits.length; i++) {
-            SearchHit hit = hits[i];
-            String _id = hit.getId();
-            String jsonStr = hit.getSourceAsString();
-            Map map = JSON.parseObject(jsonStr, Map.class);
-            map.put("_id", _id);
-            list.add(map);
+    public static List<Map<String, Object>> parseToListMap(SearchResponse response){
+        // 从response中获得结果
+        List<Map<String, Object>> list = new LinkedList<>();
+        SearchHits hits = response.getHits();
+        Iterator<SearchHit> iterator = hits.iterator();
+        while (iterator.hasNext()) {
+            SearchHit next = iterator.next();
+            list.add(next.getSourceAsMap());
+        }
+        return list;
+    }
+
+    /**
+     * 将查询结果解析成ListMaps
+     * @param response
+     * @return
+     */
+    public static List<String> parseToListStr(SearchResponse response){
+        // 从response中获得结果
+        List<String> list = new LinkedList<>();
+        SearchHits hits = response.getHits();
+        Iterator<SearchHit> iterator = hits.iterator();
+        while (iterator.hasNext()) {
+            SearchHit next = iterator.next();
+            list.add(next.getSourceAsString());
         }
         return list;
     }
